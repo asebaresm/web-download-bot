f12e8f20a6573553257f3b7489583bf81b2a148d
asebares <asebares@s21sec.com>
2019-04-07 22:42:00 +0200

    [chore] rename environment files after renaming project

--- environment/bin/activate.fish
+++ environment/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/asebares/Code/fc-no-invite-bot/environment"
+set -gx VIRTUAL_ENV "/home/asebares/Code/web-download-bot/environment"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
